efb125b289dae76712074842ea5f88850e02df7d
Grégory Houllier <gregory.houllier@gmail.com>
2024-10-02 21:46:50 +0000

    feat: implement parser

---
+++ src/parser.test.ts
@@ -0,0 +1,70 @@
+import { describe, expect, it } from "bun:test";
+import { parseContent, parseLine, isCommentLine, isEmptyLine } from "./parser";
+
+describe("parseContent()", () => {
+  it("should ignore comments", () => {
+    expect(
+      parseContent(`
+        # Hello    
+    `),
+    ).toStrictEqual([]);
+  });
+  it("should ignore comments", () => {
+    expect(
+      parseContent(`
+        # Hello    
+        package.json    @org/team
+    `),
+    ).toStrictEqual([["package.json", ["@org/team"]]]);
+  });
+  it("should support multiple owners", () => {
+    expect(
+      parseContent(`
+        # Hello    
+        package.json    @org/team1 @org/team2
+    `),
+    ).toStrictEqual([["package.json", ["@org/team1", "@org/team2"]]]);
+  });
+});
+
+describe("parseLine()", () => {
+  it("should support multiple owners", () => {
+    expect(parseLine(`package.json @org/team1 @org/team2`)).toStrictEqual([
+      "package.json",
+      ["@org/team1", "@org/team2"],
+    ]);
+  });
+  it("should support extra spaces", () => {
+    expect(parseLine(`package.json    @org/team1      @org/team2`)).toStrictEqual([
+      "package.json",
+      ["@org/team1", "@org/team2"],
+    ]);
+  });
+});
+
+describe("isCommentLine()", () => {
+  it("should support extra space", () => {
+    expect(isCommentLine(`  # Hello    `)).toBeTrue();
+  });
+  it("should support start with #", () => {
+    expect(isCommentLine(`# Hello`)).toBeTrue();
+  });
+  it("should detect valid lines", () => {
+    expect(isCommentLine(`hello`)).toBeFalse();
+  });
+  it("should detect empty lines", () => {
+    expect(isCommentLine(``)).toBeFalse();
+  });
+});
+
+describe("isEmptyLine()", () => {
+  it("should support extra space", () => {
+    expect(isEmptyLine(`  `)).toBeTrue();
+  });
+  it("should detect empty lines", () => {
+    expect(isEmptyLine(``)).toBeTrue();
+  });
+  it("should detect valid lines", () => {
+    expect(isEmptyLine(` hello  `)).toBeFalse();
+  });
+});
