6a6c8d80ea4b814ea24047b82406003b0cd86c35
Grégory Houllier <gregory.houllier@gmail.com>
2024-10-08 20:02:46 +0000

    docs: improve documentation

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,14 @@
+/**
+ * Represent a parsed line codeowners file
+ */
 export type ParsedLine = [path: string, owners: string[]];
+
+/**
+ * Represent a list of parsed line of a codeowners file
+ */
 export type Codeowners = ParsedLine[];
 
+/**
+ * A specialized class that catch parsing eror
+ */
 export class ParseError extends Error {}
